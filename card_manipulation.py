1014f11eff806be579d8066836ea7e3a41100efb
Sqidy <sqidyp@gmail.com>
2024-09-14 01:11:09 -0400

    Significantly improved locate speed, added comments

--- card_manipulation.py
+++ card_manipulation.py
@@ -2,23 +2,28 @@ import pyautogui
 
 # The entire region for the game is (1676, 167, 2522, 1485)
 tableau_region = (1676, 366, 846, 845)
-foundation_region = (1676, 185, 2155, 240)
-waste_region = (2176, 190, 2392, 253)
+foundation_region = (1676, 185, 479, 55)
+waste_region = (2176, 190, 216, 63)
 
 current_tableau = {}
 
 
-def update_cards(cards):
-    # TODO: Check entire game for card. if it exists, check if it is in the tableau or foundation.
-    # Two birds with one stone!!
+def update_cards(cards, region):
+    # Creates a PIL.Image, allowing for faster processing.
+    ss = pyautogui.screenshot(region=region)
 
     for card in cards:
         try:
-            check_for_card = pyautogui.locateCenterOnScreen(image=cards[card], region=tableau_region, confidence=0.965)
+            # Gets the coords of the image based on the region "haystackImage."
+            check_for_card = pyautogui.center(pyautogui.locate(needleImage=cards[card], haystackImage=ss, confidence=0.965))
 
-            # if region[0] <= check_for_card[0] <= region[2] and region[1] <= check_for_card[1] <= region[3]:
+            # Offsets the given coords so that it matches with the location of the card.
+            check_for_card = (check_for_card[0]+region[0], check_for_card[1]+region[1])
+
+            # Adds it to current_tableau in the form of "card" as name and "check_for_card" as coords.
             current_tableau[card] = check_for_card
 
+        # If no result is returned, just ignore it.
         except pyautogui.ImageNotFoundException:
             pass
 
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import os
 from pathlib import Path
-import multiprocessing
 
-import pyautogui
-import time
+from PIL import Image
 
 import card_manipulation
 
@@ -11,31 +9,36 @@ cards_path = "C:\\Users\\Sqidy\\PycharmProjects\\pythonProject\\card_imgs\\"
 cards = {}
 card_order = {"Red": [], "Black": []}
 
-regions = [
-    (1676, 366, 122, 845),
-    (1798, 366, 120, 845),
-    (1918, 366, 120, 845),
-    (2038, 366, 121, 845),
-    (2159, 366, 121, 845),
-    (2280, 366, 120, 845),
-    (2400, 366, 122, 845)]
-
 
 # def main():
+#     import time
+#     import pyautogui
+#
 #     while True:
 #         print(pyautogui.position())
 #         time.sleep(2)
+#     ss = pyautogui.screenshot(region=(2176, 190, 216, 63))
+#
+#     ss.save("test.png")
 
 
+# Sets the key to the images, the names, and the value which is what Image.open is returning.
 def set_card_key_to_value():
     for card in os.listdir(cards_path):
-        cards[Path(cards_path + card).stem] = cards_path + card
+        # cards[Path(cards_path + card).stem] gets the name. Image.open opens the image into memory, allowing for
+        # exponentially higher speeds, with the downside of more ram usage.
+        cards[Path(cards_path + card).stem] = Image.open("card_imgs\\"+card)
 
 
+# Sets the order of how the cards in the game are supposed to be ordered.
+# Allows for figuring out what cards go together.
 def set_card_order():
+    # Read every line in CardOrder.txt.
     for card in open("CardOrder.txt", "r"):
+        # If it's Hearts or Diamonds, put it into the Red key.
         if "Hearts" in card or "Diamonds" in card:
             card_order["Red"].append(card.strip())
+        # If it's Clubs or Spades, put it into the Black key.
         elif "Clubs" in card or "Spades" in card:
             card_order["Black"].append(card.strip())
 
@@ -44,32 +47,6 @@ if __name__ == "__main__":
     set_card_key_to_value()
     set_card_order()
 
-    card_manipulation.update_cards(cards)
+    card_manipulation.update_cards(cards, card_manipulation.tableau_region)
 
-# import multiprocessing
-# import time
-#
-#
-# def task():
-#     print('Sleeping for 0.5 seconds')
-#     time.sleep(0.5)
-#     print('Finished sleeping')
-#
-#
-# if __name__ == "__main__":
-#     start_time = time.perf_counter()
-#     processes = []
-#
-#     # Creates 10 processes then starts them
-#     for i in range(10):
-#         p = multiprocessing.Process(target=task)
-#         p.start()
-#         processes.append(p)
-#
-#     # Joins all the processes
-#     for p in processes:
-#         p.join()
-#
-#     finish_time = time.perf_counter()
-#
-#     print(f"Program finished in {finish_time - start_time} seconds")
+    # main()
